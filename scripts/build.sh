cba5451be8c3ee8249843b58723a31ea48c7bf2a
David Alger <davidmalger@gmail.com>
2020-03-06 10:20:43 -0600

    Added IonCube loader to CLI image variant used for pipelines and changed to 'loaders' suffix; added fpm variant with loaders

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,15 +28,12 @@ fi
 
 ## iterate over and build each version/variant combination
 VERSION_LIST="${VERSION_LIST:-"55 56 70 71 72 73"}"
-VARIANT_LIST="${VARIANT_LIST:-"cli fpm sourceguardian"}"
+VARIANT_LIST="${VARIANT_LIST:-"cli cli-loaders fpm fpm-loaders"}"
 
 for PHP_VERSION in ${VERSION_LIST}; do
   for PHP_VARIANT in ${VARIANT_LIST}; do
     IMAGE_TAG="davidalger/php:$(echo ${PHP_VERSION} | sed -E 's/([0-9])([0-9])/\1.\2/')"
-    if [[ ${PHP_VARIANT} != "cli" ]]; then
-      IMAGE_TAG+="-${PHP_VARIANT}"
-    fi
-
+    PHP_VARIANT="$(echo ${PHP_VARIANT} | sed 's/^cli-//')"
     export PHP_VERSION
 
     printf "\e[01;31m==> building ${IMAGE_TAG}\033[0m\n"
