81df53f012a513a533955525c763a54a8a419eee
David Alger <davidmalger@gmail.com>
2020-04-26 21:13:29 -0500

    Copied in build script from gist
    https://gist.github.com/davidalger/cdc56efb1bbb1140d2315a355379bafc

---
+++ scripts/build-php74-rpms.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+set -euo pipefail
+
+RELEASEVER=7
+PKG_LIST=(
+  iusrepo/libzip1
+  kelnei/oniguruma6
+  davidalger/php74
+  iusrepo/pear1
+  kelnei/php74-pecl-apcu
+  kelnei/php74-pecl-igbinary
+  kelnei/php74-pecl-msgpack
+  kelnei/php74-pecl-redis
+)
+
+yum install -y wget unzip
+yum --assumeyes install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
+yum --assumeyes install yum-utils rpmdevtools @buildsys-build
+
+for PKG_REPO in ${PKG_LIST[@]}; do
+  echo "==> Building $PKG_REPO"
+  PKG_NAME=$(echo $PKG_REPO | cut -d/ -f2)
+
+  wget https://github.com/$PKG_REPO/archive/master.zip -O $PKG_NAME.zip
+  unzip -d / $PKG_NAME.zip && cd /$PKG_NAME-master
+
+  cat > $HOME/.rpmmacros <<-EOT
+		%_sourcedir $PWD
+		%_specdir $PWD
+		%dist .el$RELEASEVER.ius
+		%vendor IUS
+	EOT
+
+  spectool --get-files $PKG_NAME.spec
+  rpmbuild -bs $PKG_NAME.spec
+  yum-builddep --assumeyes $HOME/rpmbuild/SRPMS/$PKG_NAME-*.src.rpm
+  rpmbuild -bb $PKG_NAME.spec
+
+  PKG_BUILT=$(ls -1 $HOME/rpmbuild/SRPMS/*$PKG_NAME*.src.rpm $HOME/rpmbuild/RPMS/*/*$PKG_NAME*.rpm)
+  echo "==> Installing $PKG_BUILT"
+  yum install -y $PKG_BUILT
+done
+
+echo "==> Build Artifacts"
+ls -1 $HOME/rpmbuild/SRPMS/*.src.rpm $HOME/rpmbuild/RPMS/*/*.rpm | sort
