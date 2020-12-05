8842e6133c5892d9c3e5bfc4a8388bca14423ad4
David Alger <davidmalger@gmail.com>
2020-12-05 16:12:19 -0600

    Updated build script to be more robust and to tag both minor and major versions on image

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
-trap '>&2 printf "\n\e[01;31mError: Command \`%s\` on line $LINENO failed with exit code $?\033[0m\n" "$BASH_COMMAND"' ERR
+trap 'error "$(printf "Command \`%s\` at $BASH_SOURCE:$LINENO failed with exit code $?" "$BASH_COMMAND")"' ERR
+
+function error {
+  >&2 printf "\033[31mERROR\033[0m: %s\n" "$@"
+}
 
 ## find directory above where this script is located following symlinks if neccessary
 readonly BASE_DIR="$(
@@ -12,7 +16,7 @@ readonly BASE_DIR="$(
   )/.." >/dev/null \
   && pwd
 )"
-pushd ${BASE_DIR} >/dev/null
+pushd "${BASE_DIR}" >/dev/null
 
 ## if --push is passed as first argument to script, this will login to docker hub and push images
 PUSH_FLAG=
@@ -22,8 +26,11 @@ fi
 
 ## login to docker hub as needed
 if [[ $PUSH_FLAG ]]; then
-  [ -t 1 ] && docker login \
-    || echo "${DOCKER_PASSWORD:-}" | docker login -u "${DOCKER_USERNAME:-}" --password-stdin
+  if [ -t 1 ]; then
+    docker login
+  else
+    echo "${DOCKER_PASSWORD:-}" | docker login -u "${DOCKER_USERNAME:-}" --password-stdin
+  fi
 fi
 
 ## iterate over and build each version/variant combination; by default building
@@ -31,19 +38,39 @@ fi
 VERSION_LIST="${VERSION_LIST:-"7.3"}"
 VARIANT_LIST="${VARIANT_LIST:-"cli cli-loaders fpm fpm-loaders"}"
 
-for PHP_VERSION in ${VERSION_LIST}; do
-  for PHP_VARIANT in ${VARIANT_LIST}; do
-    IMAGE_TAG="davidalger/php:$(echo ${PHP_VERSION} | sed -E 's/([0-9])([0-9])/\1.\2/')"
-    IMAGE_TAG_SUFFIX="$(echo ${PHP_VARIANT} | sed -E 's/^(cli$|cli-)//')"
-    if [[ ${IMAGE_TAG_SUFFIX} ]]; then
-      IMAGE_TAG+="-${IMAGE_TAG_SUFFIX}"
-    fi
+IMAGE_NAME="davidalger/php"
+for BUILD_VERSION in ${VERSION_LIST}; do
+  MAJOR_VERSION="$(echo "${BUILD_VERSION}" | sed -E 's/([0-9])([0-9])/\1.\2/')"
+  for BUILD_VARIANT in ${VARIANT_LIST}; do
+    # Configure build args specific to this image build
+    export PHP_VERSION="${MAJOR_VERSION}"
+    BUILD_ARGS=(PHP_VERSION)
 
-    export PHP_VERSION
+    # Build the image passing list of tags and build args
+    printf "\e[01;31m==> building %s:%s (%s)\033[0m\n" \
+      "${IMAGE_NAME}" "${BUILD_VERSION}" "${BUILD_VARIANT}"
 
-    printf "\e[01;31m==> building ${IMAGE_TAG}\033[0m\n"
-    docker build -t "${IMAGE_TAG}" --build-arg PHP_VERSION ${BASE_DIR}/${PHP_VARIANT}
+    docker build -t "${IMAGE_NAME}:build" "${BUILD_VARIANT}" $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}")
 
-    [[ $PUSH_FLAG ]] && docker push "${IMAGE_TAG}"
+    # Strip the term 'cli' from tag suffix as this is the default variant
+    TAG_SUFFIX="$(echo "${BUILD_VARIANT}" | sed -E 's/^(cli$|cli-)//')"
+    [[ ${TAG_SUFFIX} ]] && TAG_SUFFIX="-${TAG_SUFFIX}"
+
+    # Fetch the precise php version from the built image and tag it
+    MINOR_VERSION="$(docker run --rm -t --entrypoint php "${IMAGE_NAME}:build" -r 'echo phpversion();')"
+
+    # Generate array of tags for the image being built
+    IMAGE_TAGS=(
+      "${IMAGE_NAME}:${MAJOR_VERSION}${TAG_SUFFIX}"
+      "${IMAGE_NAME}:${MINOR_VERSION}${TAG_SUFFIX}"
+    )
+
+    # Iterate and push image tags to remote registry
+    for TAG in "${IMAGE_TAGS[@]}"; do
+      docker tag "${IMAGE_NAME}:build" "${TAG}"
+      echo "Successfully tagged ${TAG}"
+      [[ $PUSH_FLAG ]] && docker push "${TAG}"
+    done
+    docker image rm "${IMAGE_NAME}:build"
   done
 done
